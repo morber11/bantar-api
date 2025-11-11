b42a8012bf0af754e336104bca190c02dd0716ee
morber <4497491+morber11@users.noreply.github.com>
2025-11-11 12:34:16 +0000

    remove ICEBREAKER from IcebreakerCategory

--- src/main/java/com/bantar/model/IcebreakerCategory.java
+++ src/main/java/com/bantar/model/IcebreakerCategory.java
@@ -5,7 +5,6 @@ import com.bantar.util.EnumUtils;
 import java.util.List;
 
 public enum IcebreakerCategory {
-    ICEBREAKER("Icebreakers"),
     CASUAL("Casual"),
     ROMANTIC("Romantic"),
     NSFW("Not Safe For Work/18+"),
