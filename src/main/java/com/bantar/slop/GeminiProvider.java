298786824ff6de58bf85856238f951d04f38494c
morber <4497491+morber11@users.noreply.github.com>
2026-03-06 20:28:52 +0000

    add logging for GeminiProvider

--- src/main/java/com/bantar/slop/GeminiProvider.java
+++ src/main/java/com/bantar/slop/GeminiProvider.java
@@ -2,19 +2,30 @@ package com.bantar.slop;
 
 import com.google.genai.Client;
 import com.google.genai.types.GenerateContentResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
 
 import static com.bantar.config.Constants.GEMINI_MODEL;
 
 @Service
 public class GeminiProvider implements SlopProvider {
 
+    private static final Logger logger = LogManager.getLogger(GeminiProvider.class);
     private final Client aiClient;
 
     @Autowired
     public GeminiProvider(Client aiClient) {
         this.aiClient = aiClient;
+        logger.info("GeminiProvider constructed with client {}", aiClient);
+    }
+
+    @PostConstruct
+    void started() {
+        logger.info("GeminiProvider bean initialized");
     }
 
     @Override
