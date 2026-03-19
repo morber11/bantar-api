307f87840160c7f1c65b767dab358c78ca7ba080
morber <4497491+morber11@users.noreply.github.com>
2026-03-19 13:06:45 +0000

    disable swagger in prod

--- src/main/java/com/bantar/controller/RedirectController.java
+++ src/main/java/com/bantar/controller/RedirectController.java
@@ -1,6 +1,7 @@
 package com.bantar.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,11 +13,15 @@ import org.slf4j.LoggerFactory;
 @Controller
 public class RedirectController {
 
+    @Value("${springdoc.swagger-ui.enabled:true}")
+    private boolean swaggerEnabled;
+
     private static final Logger logger = LoggerFactory.getLogger(RedirectController.class);
 
     @RequestMapping("/")
     public RedirectView redirectToSwagger(HttpServletRequest request, HttpServletResponse response) {
-        RedirectView redirectView = new RedirectView("/api/swagger-ui.html");
+        String target = swaggerEnabled ? "/api/swagger-ui.html" : "/api/health";
+        RedirectView redirectView = new RedirectView(target);
 
         logger.info("Request URL: {} Method: {} Status: {}",
                 request.getRequestURL().toString(), request.getMethod(), response.getStatus());
